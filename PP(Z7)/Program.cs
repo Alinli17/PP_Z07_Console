a1b09dc649d4b1b3c04b8fe3ff21d3b8f13ff9bc
alina.timonina172372 <alina.timonina172372@gmail.com>
2020-05-15 18:34:47 +0300

    Добавьте файлы проекта.

---
+++ PP(Z7)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Z7_
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку str: ");
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            Console.Write("Введите подстроку substr: ");
+            StringBuilder substr = new StringBuilder(Console.ReadLine());
+            str.Replace(substr.ToString(), "");
+            Console.WriteLine("Измененная строка str: {0}", str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PP(Z7_1)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Z7_1_
+{
+    class Program
+    {
+        static int numberOfUpWords(StringBuilder input)
+        {
+            int ret = 0, ind = 0;
+            while (ind < input.Length)
+            {
+                int nUCase = 0, nLCase = 0;
+                while (ind < input.Length && char.IsLetter(input[ind]))
+                {
+                    if (char.IsUpper(input[ind])) nUCase++;
+                    if (char.IsLower(input[ind])) nLCase++;
+                    ind++;
+                }
+                if (nUCase > 0 && nLCase == 0) ret++;
+                while (ind < input.Length && !char.IsLetter(input[ind]))
+                    ind++;
+            }
+            return ret;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку str: ");
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            Console.WriteLine("Кол-во слов, содержащие только прописные буквы: " + numberOfUpWords(str));
+            Console.ReadLine();
+        }
+    }
+}
